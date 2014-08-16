989e9d686960be972f2839af862684d45ece611f
steenuil <steenuil@olivia.local>
2014-08-16 02:11:22 +0200

    Fixed sha1

--- Formula/pearpc.rb
+++ Formula/pearpc.rb
@@ -3,7 +3,7 @@ require "formula"
 class Pearpc < Formula
   homepage "http://pearpc.sourceforge.net"
   url "https://downloads.sourceforge.net/project/pearpc/pearpc/0.5.0/pearpc-0.5.tar.bz2"
-  sha1 "a4c37a93e5037a6934ead188b6421e9ea486f92e"
+  sha1 "17c5ec69f0139d5a5ce02186a3c990040f45f343"
 
   depends_on :arch => :intel
   depends_on "sdl" if build.without? :x11
