6d408c7ff110578814fb43a0870e4321fb94faca
steinuil <steenuil.owl@gmail.com>
2014-12-28 01:37:54 +0100

    updated existing formulae to new specs, added noice

--- Formula/teeworlds.rb
+++ Formula/teeworlds.rb
@@ -1,21 +1,19 @@
-require 'formula'
-
 class Teeworlds < Formula
-  homepage 'https://www.teeworlds.com'
-  url 'https://downloads.teeworlds.com/teeworlds-0.6.3-src.tar.gz'
-  sha1 'f2dea0d14c4a8c817c658bd75dbe175b37ed70bd'
+  homepage "https://www.teeworlds.com"
+  url "https://downloads.teeworlds.com/teeworlds-0.6.3-src.tar.gz"
+  sha1 "f2dea0d14c4a8c817c658bd75dbe175b37ed70bd"
 
-  depends_on 'sdl'
-  depends_on 'bam' => :build
+  depends_on "sdl"
+  depends_on "bam" => :build
 
   def install
     system "bam release"
-    (bin+'teeworlds').write <<-EOS.undent
+    (bin+"teeworlds").write <<-EOS.undent
       #!/bin/sh
       exec #{libexec}/teeworlds
     EOS
     rm_rf "src"
     rm_rf "objs"
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
   end
 end
